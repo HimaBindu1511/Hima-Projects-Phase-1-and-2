f677b603ba739c50399fbdad13243274a162e25f
Hima Bindu Appireddy <67799179+HimaBindu1511@users.noreply.github.com>
2023-07-12 11:36:20 +0530

    Add files via upload

---
+++ SortingConcepts/SelectionSorting.java
@@ -0,0 +1,25 @@
+package SortingConcepts;
+
+import java.util.Arrays;
+public class SelectionSorting
+{
+	
+	// Driver code
+	public static void main(String args[])
+	{int temp;
+		int arr[] = {2, 13, 4, 10, 4};
+		for(int i=0;i<arr.length-1;i++){
+		    for(int j=i+1;j<arr.length;j++){
+		      if(arr[i]<arr[j]){
+		          temp=arr[i];
+		          arr[i]=arr[j];
+		          arr[j]=temp;
+		      }  
+		    }
+		}
+		
+		//sorted Arrays
+		for(int i=0;i<arr.length;i++)
+		System.out.println(arr[i]);
+	}
+}
