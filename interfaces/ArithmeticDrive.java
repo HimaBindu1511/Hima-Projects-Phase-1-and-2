00b7a19e885367c20e0c3c99bef9c97aa2cc0e32
Hima Bindu Appireddy <67799179+HimaBindu1511@users.noreply.github.com>
2023-07-12 11:20:38 +0530

    Add files via upload

---
+++ interfaces/ArithmeticDrive.java
@@ -0,0 +1,25 @@
+package interfaces;
+
+public class ArithmeticDrive {
+
+	public static void main(String[] args) {
+		ArithmeticDrive obj=new ArithmeticDrive();
+		System.out.println("Sum="+obj.addition(56, 45));
+		System.out.println("Difference="+obj.subtraction(23, 15));
+
+	}
+
+	private String subtraction(int i, int j) {
+		ArithmeticDrive obj = null;
+		// TODO Auto-generated method stub
+		System.out.println("Sum="+obj.addition(56, 45));
+		return null;
+
+	}
+
+	private String addition(int i, int j) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
